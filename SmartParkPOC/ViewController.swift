814eb6ebaf2bd21f91186af5808fe445a87a79c2
Hong Zhang <hong_zhang@foxitsoftware.com>
2015-08-27 12:48:26 -0700

    Initial Commit

---
+++ SmartParkPOC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartParkPOC
+//
+//  Created by Change to ur account when u use on 8/27/15.
+//  Copyright (c) 2015 com.hong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
